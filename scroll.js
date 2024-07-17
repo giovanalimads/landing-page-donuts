4345659e6a01c9ffcfab544c3e6fc9409ae919ae
giovanalimads <gilimaolliveira@gmail.com>
2024-07-16 21:51:07 -0300

    commit

---
+++ scroll.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('a[href^="#products"]');
+
+  links.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetId = link.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  });
+});
